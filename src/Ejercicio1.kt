1a561dbb9c01bcc873fa26ad306e4e90aabd3226
Cristina <bau161260@uvg.edu.gt>
2018-07-09 16:04:58 -0600

    Agregue codigo para que imprima las categorias

--- src/Ejercicio1.kt
+++ src/Ejercicio1.kt
@@ -2,20 +2,40 @@ package main
 
 
 class Default(
-        private val Estado: String,
-        private val Volumen: Int,
-        private val Estacion: String,
-        private val Estado2: String,
-)
+        private var Estado: String,
+        private var Volumen: Int,
+        private var Estacion: String,
+        private var Estado2: String
+) {
+    fun getEstado(): String {
+        return Estado
+    }
+
+    fun getVolumen(): Int {
+        return Volumen
+    }
+
+    fun retornarVolumen():Int{
+      return Volumen
+    }
+
+    fun getEstacion():String{
+      return Estacion
+    }
+    fun getEstado2():String{
+      return Estado2
+    }
+}
 fun main(args : Array<String>) {
 
     val radioDefault = Default(
-            Estado = "apagado",
-            Volumen = 2,
+            Estado = "Apagado",
+            Volumen = 1,
             Estacion = "FM",
-            Estado2 = "salir"
+            Estado2 = "Salir"
     )
-
-    println(radioDefault)
-
+    println(radioDefault.getEstado())
+    println(radioDefault.getVolumen())
+    println(radioDefault.getEstacion())
+    println(radioDefault.getEstado2())
 }
